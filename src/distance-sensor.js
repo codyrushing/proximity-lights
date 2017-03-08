451fa8d2b6a61665bfc72fa78cff923ad4ff25a3
Cody Rushing <codyrushing@gmail.com>
2017-03-07 20:39:29 -0600

    setting up light channel, begin events logic

--- src/distance-sensor.js
+++ src/distance-sensor.js
@@ -5,17 +5,20 @@ Hue System API:
 https://developers.meethue.com/documentation/core-concepts
 */
 
+const range = [6, 254];
+
 const SerialPort = require('serialport');
 class DistanceSensor {
-  constructor(params){
-    Object.keys(params).forEach(key => {
-      this[key] = params[key];
-    });
+  constructor({serialPath, lightId}){
+    this.serialPath = serialPath;
+    this.lightId = lightId;
     this.init();
   }
 
   init(){
     this.vals = [];
+    this.initLightChannel(this.lightId);
+
     this.port = new SerialPort(this.serialPath,{
       // this sensor prepends 'R' before each value
       parser: SerialPort.parsers.readline('R')
@@ -26,6 +29,12 @@ class DistanceSensor {
       .on('data', data => this.on_data(data));
   }
 
+  initLightChannel(lightId){
+    this.lightChannel = new LightChannel({
+      lightId: lightId
+    });
+  }
+
   on_data(data){
     const distance = parseInt(data, 10);
     // save up to 100 values, which corresponds to 10 secs of data
@@ -33,9 +42,29 @@ class DistanceSensor {
     this.update();
   }
 
+  triggerEvent(event, data){
+    if(this.lightChannel){
+      this.lightChannel.emit(event, data);
+    }
+  }
+
   update(){
-    //
-    console.log(d3Array.variance(this.vals));
+    // is still
+    // is moving
+    // enter
+    // exit
+    const movementThreshold = 30;
+    const twoSampleAvg = d3Array.mean(this.vals.slice(0,2));
+    const priorTwoSampleAvg = d3Array.mean(this.vals.slice(2,4));
+    const velocity = (twoSampleAvg - priorTwoSample)/0.2;
+    if(velocity > movementThreshold){
+      // exit
+      this.triggerEvent('exit');
+    } else if(velocity < movementThreshold*-1) {
+      // enter
+      this.triggerEvent('enter');
+    }
+
   }
 
 }
--- src/light-channel.js
+++ src/light-channel.js
@@ -1,11 +1,27 @@
 const EventEmitter = require('events');
+const request = require('request');
 const config = require('./config');
 
+const { bridgeHost, bridgeUser} = config;
+
 class LightChannel extends EventEmitter {
-  constructor(){
+  constructor({lightId}){
+    this.lightId = lightId;
     this.init();
   }
   init(){
+    // bind events
+    this.on('update', this.update())
+  }
+  update(){
+    request({
+      method: 'PUT',
+      url: `http://${bridgeHost}/api/${bridgeUser}/lights/${this.lightId}/state`,
+      json: true,
+      body: {
+        transitiontime: 0
+      }
+    });
 
   }
 }
