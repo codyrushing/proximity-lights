119fc31da360e26d751a1cdb7427c90a6cb40f99
Cody Rushing <codyrushing@gmail.com>
2017-03-29 21:58:14 -0500

    more fleshed out logic, still getting double events

--- src/distance-sensor.js
+++ src/distance-sensor.js
@@ -7,6 +7,9 @@ https://developers.meethue.com/documentation/core-concepts
 */
 
 const range = [6, 254];
+const sampleSize = 2;
+const sampleRate = 0.1;
+const movementThreshold = 90; // inches per second
 
 const SerialPort = require('serialport');
 class DistanceSensor {
@@ -17,7 +20,7 @@ class DistanceSensor {
   }
 
   init(){
-    this.still = true;
+    this.setInitialState();
     this.vals = [];
     this.initLightChannel(this.lightId);
 
@@ -31,6 +34,11 @@ class DistanceSensor {
       .on('data', data => this.on_data(data));
   }
 
+  setInitialState(){
+    this.isMoving = false;
+    this.isEmpty = true;
+  }
+
   initLightChannel(lightId){
     this.lightChannel = new LightChannel({
       lightId: lightId
@@ -39,45 +47,74 @@ class DistanceSensor {
 
   on_data(data){
     const distance = parseInt(data, 10);
-    // save up to 100 values, which corresponds to 10 secs of data
-    this.vals = [distance].concat(this.vals).slice(0,100);
-    this.update();
+    if(!isNaN(distance)){
+      // save up to 100 values, which corresponds to 10 secs of data
+      this.vals = [distance].concat(this.vals).slice(0,100);
+      setTimeout(this.update.bind(this),0);
+    }
+    // console.log(distance);
   }
 
   triggerEvent(event, data){
+    console.log(event);
     if(this.lightChannel){
       this.lightChannel.emit(event, data);
     }
   }
 
-  update(){
-    // is still
-    // is moving
-    // enter
-    // exit
-    const movementThreshold = 90;
-    const latestDistance = d3Array.mean(this.vals.slice(0,2));
-    const prevDistance = d3Array.mean(this.vals.slice(2,4));
-
-    // calculate velocity in inches per second
-    const velocity = (latestDistance - prevDistance)/0.2;
-    if(velocity > movementThreshold){
-      // exit
-      // console.log(`velocity = ${velocity}`);
-      this.triggerEvent('exit');
-    } else if(velocity < movementThreshold*-1) {
-      // enter
-      // console.log(`velocity = ${velocity}`);
+  on_enter(){
+    if(this.isEmpty){
       this.triggerEvent('enter');
+      this.isEmpty = false;
     }
+  }
 
-    // movement needs to be more sensitive that stillness, listens to last 4 readings
-    if(d3Array.deviation(this.vals.slice(0,4)) >= 1 && this.still) {
-      console.log('movement');
-      this.still = false;
-    } else if(!this.still && d3Array.deviation(this.vals.slice(0,10)) < 0.5){
-      console.log('stillness');
-      this.still = true;
+  on_exit(){
+    if(!this.isEmpty){
+      this.triggerEvent('exit');
+      this.isEmpty = true;
+    }
+  }
+
+  on_movement(movementFactor){
+    this.isEmpty = false;
+    this.isMoving = true;
+    this.triggerEvent('movement', movementFactor);
+  }
+
+  on_stillness(){
+    this.isMoving = false;
+    this.triggerEvent('stillness')
+  }
+
+  update(){
+    const latestDistance = d3Array.mean(this.vals.slice(0,sampleSize));
+    const prevDistance = d3Array.mean(this.vals.slice(sampleSize,sampleSize*2));
+    // calculate velocity in inches per second
+    const velocity = (latestDistance - prevDistance)/(sampleSize * sampleRate);
+    // calculate movementFactor from variance
+    const movementFactor = d3Array.variance(this.vals.slice(0,sampleSize*2))
+    /*
+    if empty
+      listen for enter event
+      listen for movement, which should also trigger an enter
+    else if not empty
+      listen for exit event
+      listen for stillness
+    */
+
+    if(this.isEmpty){
+      if(velocity < -movementThreshold){
+        this.on_enter();
+      }
+    } else {
+      if(velocity > movementThreshold){
+        this.on_exit();
+      } else if(this.isMoving && d3Array.variance(this.vals.slice(0,sampleSize*5)) < 0.2){
+        this.on_stillness();
+      } else if(movementFactor >= 1){
+        this.on_movement();
+      }
     }
 
   }
