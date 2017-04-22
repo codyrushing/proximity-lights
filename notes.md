62e0db755c84f692b2d1bab41231508e082a58b1
Cody Rushing <codyrushing@gmail.com>
2017-04-22 09:54:05 -0500

    WIP

--- notes.md
+++ notes.md
@@ -2,3 +2,9 @@
 * There may be something about the deviation algorithm I'm using to determine movement factor that returns larger or smaller number based not on movement but on distance from sensor.
 * Gracing the edge of the sensing field sort of causes problems.  Perhaps mitigate this by checking for super bouncy values.
 * Difficult to distinguish between a sharp sudden movement and a person walking in front.  Currently, a person has to walk about 4 - 5 feet in front of another person to trigger an enter event.
+
+### Philips Hue API notes
+* There's a convoluted `xy` option, but it will likely be easier to use `hue`, `sat`, and `bri`.
+* `ct` is color temperature, so warm and cold whites.  Maybe good for a non color-heavy recipe.
+* `"effect": "colorloop"` cycles through all colors.  `"alert": "select"` blinks the last color.
+* You can address all lights with `/groups/0`
--- src/distance-sensor.js
+++ src/distance-sensor.js
@@ -20,6 +20,14 @@ class DistanceSensor {
     this.init();
   }
 
+  getState(){
+    var state = {};
+    ['distance', 'velocity', 'occupantsCount', 'isMoving', 'isEmpty', 'movementLong', 'movementShort'].forEach(prop => {
+      state[prop] = this[prop]
+    });
+    return state;
+  }
+
   init(){
     this.setInitialState();
     this.vals = [];
@@ -36,9 +44,13 @@ class DistanceSensor {
   }
 
   setInitialState(){
+    this.velocity = 0;
+    this.distance = 255;
     this.occupantsCount = 0;
     this.isMoving = false;
     this.isEmpty = true;
+    this.movementShort = 0;
+    this.movementLong = 0;
     this.exitTime = Date.now();
   }
 
@@ -54,8 +66,16 @@ class DistanceSensor {
       // save up to 100 values, which corresponds to 10 secs of data
       this.vals = [distance].concat(this.vals).slice(0,100);
       this.update();
+      process.nextTick(this.send);
     }
-    // console.log(distance);
+  }
+
+  send(){
+    if(this.lightChannel){
+      this.lightChannel.emit('state', this.getState());
+    } else {
+      console.error('No lightChannel found');
+    }
   }
 
   triggerEvent(event, data){
@@ -81,7 +101,7 @@ class DistanceSensor {
 
   on_movement(movementFactor){
     this.isMoving = true;
-    this.triggerEvent('movement', movementFactor);
+    this.triggerEvent('move', movementFactor);
   }
 
   on_stillness(){
@@ -100,16 +120,13 @@ class DistanceSensor {
 
   update(){
     const now = Date.now();
-    const latestDistance = d3Array.mean(this.vals.slice(0,sampleSize));
     const prevDistance = d3Array.mean(this.vals.slice(sampleSize,sampleSize*2));
     // calculate velocity in inches per second
-    const velocity = (latestDistance - prevDistance)/(sampleSize * sampleRate);
+    this.distance = d3Array.mean(this.vals.slice(0,sampleSize));
+    this.velocity = (this.distance - prevDistance)/(sampleSize * sampleRate);
     // calculate movementFactor from variance
-    const shortTermMovementFactor = Math.log(d3Array.variance(this.vals.slice(0,sampleSize*2)) + 1);
-    const longTermMovementFactor = Math.log(d3Array.variance(this.vals.slice(0,sampleSize*4)) + 1);
-
-    // console.log(latestDistance);
-    console.log(this.getMovementFactor(this.vals.slice(0, 4)));
+    this.movementShort = this.getMovementFactor(this.vals.slice(0,sampleSize*2));
+    this.movementLong = this.getMovementFactor(this.vals.slice(0,sampleSize*4));
 
     /*
     if empty
@@ -120,24 +137,23 @@ class DistanceSensor {
       listen for stillness
     */
 
-    // console.log(shortTermMovementFactor);
-
     if(this.isEmpty){
-      if(velocity < -movementThreshold){
+      if(this.velocity < -movementThreshold){
         this.on_enter();
       }
       // long term movement factor
-      if(now - this.exitTime > 1000 && longTermMovementFactor >= 1 && longTermMovementFactor < 200){
+      // TODO - adjust these constants to reflect new movement algorithm
+      if(now - this.exitTime > 1000 && this.movementLong >= 1 && this.movementLong < 200){
         this.on_enter();
-        this.on_movement(shortTermMovementFactor);
+        this.on_movement(this.movementShort);
       }
     } else {
-      if(velocity > movementThreshold){
+      if(this.velocity > movementThreshold){
         this.on_exit();
-      } else if(this.isMoving && shortTermMovementFactor < 0.2){
+      } else if(this.isMoving && this.movementShort < 0.2){
         this.on_stillness();
-      } else if(shortTermMovementFactor >= 1){
-        this.on_movement(shortTermMovementFactor);
+      } else if(this.movementShort >= 1){
+        this.on_movement(this.movementShort);
       }
     }
 
